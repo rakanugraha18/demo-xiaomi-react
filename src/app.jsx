8792a550556d388bb9a7f3f32913a694b1a05671
rakanugraha18 <rakanugraha2@gmail.com>
2023-11-25 12:36:49 +0700

    remove navbar

--- src/app.jsx
+++ src/app.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <Router>
-        <Header />
+        {/* <Header /> */}
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/poco" element={<Poco />} />
